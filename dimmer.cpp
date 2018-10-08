4f8e7d597e9c208254de017716a5e2e65c5ba0d4
jeroenst <jst@dse.nl>
2018-10-08 09:55:46 +0200

    When lamp was full on it had flickering, fixed. Added setstate and getstate

--- dimmer.cpp
+++ dimmer.cpp
@@ -1,22 +1,23 @@
 #include <ESP8266WiFi.h>
 #include "hw_timer.h"
 
-uint8_t dimmer_fade = 1;
-uint8_t dimmer_state = 1;
-uint8_t dimmer_tarBrightness = 255;
+bool dimmer_fade = 1;
+bool dimmer_state = 1;
+uint8_t dimmer_setBrightness = 0;
+uint8_t dimmer_tarBrightness = 0;
 uint8_t dimmer_curBrightness = 0;
 uint16_t dimmer_dimDelay = 0;
 uint8_t dimmer_zerocrosspin = 0;
 uint8_t dimmer_triacpin = 0;
 uint8_t dimmer_fadedelay = 0;
 
-
 void dimmer_dimTimerISR();
 void dimmer_zcDetectISR();
 
 void dimmer_setdimvalue(byte value)
 {
-  dimmer_tarBrightness = value;
+  dimmer_setBrightness = value;
+  if (dimmer_state) dimmer_tarBrightness = dimmer_setBrightness;
 }
 
 uint8_t dimmer_getdimvalue()
@@ -24,11 +25,24 @@ uint8_t dimmer_getdimvalue()
   return dimmer_tarBrightness;
 }
 
-void dimmer_init(uint8_t zerocrosspin, uint8_t triacpin)
+void dimmer_setstate(bool value)
+{
+  dimmer_state = value;
+  if (dimmer_state) dimmer_tarBrightness = dimmer_setBrightness;
+  else dimmer_tarBrightness = 0;
+}
+
+bool dimmer_getstate()
+{
+  return dimmer_state;
+}
+
+void dimmer_init(uint8_t zerocrosspin, uint8_t triacpin, bool fade = true)
 {
   dimmer_zerocrosspin = zerocrosspin;
   dimmer_triacpin = triacpin;
-//  system_update_cpu_freq(160); // Minimize flickering of lamp
+  dimmer_fade = fade;
+  //  system_update_cpu_freq(160); // Minimize flickering of lamp
   pinMode(dimmer_zerocrosspin, INPUT_PULLUP);
   digitalWrite(dimmer_triacpin, 0);
   pinMode(dimmer_triacpin, OUTPUT);
@@ -40,54 +54,45 @@ void dimmer_init(uint8_t zerocrosspin, uint8_t triacpin)
 
 void dimmer_stop()
 {
-    detachInterrupt(dimmer_zerocrosspin);
-    hw_timer_set_func(0);
-    hw_timer_deinit();
-    digitalWrite(dimmer_triacpin, 0);
+  detachInterrupt(dimmer_zerocrosspin);
+  hw_timer_set_func(0);
+  hw_timer_deinit();
+  digitalWrite(dimmer_triacpin, 0);
 }
 
-void dimmer_zcDetectISR() 
+void dimmer_zcDetectISR()
 {
-  if (dimmer_curBrightness < 255) {
+  digitalWrite(dimmer_triacpin, 0);
+  if ((dimmer_curBrightness > 0) || (dimmer_tarBrightness > 0))
+  {
     hw_timer_arm(dimmer_dimDelay);
   }
-  else digitalWrite(dimmer_triacpin, 1);
 }
 
 
-void dimmer_dimTimerISR() 
+void dimmer_dimTimerISR()
 {
-    digitalWrite(dimmer_triacpin, 1);
-    static bool turnoff = 0;
-    if (turnoff == 1)
-    {
-      digitalWrite(dimmer_triacpin, 0);
-      turnoff = 0;
-      return;      
-    }
-    turnoff = 1;
+  digitalWrite(dimmer_triacpin, 1);
 
-    if (dimmer_fadedelay < 1) dimmer_fadedelay++;
-    else
-    {
-      dimmer_fadedelay = 0;
-      if (dimmer_fade == 1) {
-        if (dimmer_curBrightness > dimmer_tarBrightness && dimmer_curBrightness > 0) {
-          --dimmer_curBrightness;
-        }
-        else if (dimmer_curBrightness < dimmer_tarBrightness && dimmer_curBrightness < 255 ) {
-          if ((dimmer_curBrightness == 0) && (dimmer_tarBrightness > 15)) dimmer_curBrightness = 15;
-          ++dimmer_curBrightness;
-        }
+  if (dimmer_fadedelay < 1) dimmer_fadedelay++;
+  else
+  {
+    dimmer_fadedelay = 0;
+    if (dimmer_fade == 1) {
+      if (dimmer_curBrightness > dimmer_tarBrightness && dimmer_curBrightness > 0) {
+        --dimmer_curBrightness;
+      }
+      else if (dimmer_curBrightness < dimmer_tarBrightness && dimmer_curBrightness < 255 ) {
+        if ((dimmer_curBrightness == 0) && (dimmer_tarBrightness > 15)) dimmer_curBrightness = 15;
+        ++dimmer_curBrightness;
       }
     }
+  }
 
-    if (dimmer_fade == 0)
-    {
-        dimmer_curBrightness = dimmer_tarBrightness;
-    }
+  if (dimmer_fade == 0)
+  {
+    dimmer_curBrightness = dimmer_tarBrightness;
+  }
 
-    dimmer_dimDelay = 30 * (255 - dimmer_curBrightness) + 400;
-
-    hw_timer_arm(100);
+  dimmer_dimDelay = (30 * (255 - dimmer_curBrightness)) + 400;
 }
--- dimmer.h
+++ dimmer.h
@@ -1,4 +1,8 @@
 void dimmer_setdimvalue(byte value);
 uint8_t dimmer_getdimvalue();
-void dimmer_init(uint8_t zerocrosspin, uint8_t triacpin);
+
+void dimmer_setstate(bool value);
+bool dimmer_getstate();
+
+void dimmer_init(uint8_t zerocrosspin, uint8_t triacpin, bool fade = true);
 void dimmer_stop();
