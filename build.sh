9c1175dbbbaa11bfba9d7b372055a55ae35f56a6
jeroenst <jst@dse.nl>
2019-12-14 11:56:34 +0100

    Build now uses git tags for version instead of version file

--- build.sh
+++ build.sh
@@ -22,19 +22,19 @@ increment_version ()
 build ()
 {
   echo ''
-  echo '########################################################'
+  echo '###################################################################################################'
   echo BUILDING $targetname VERSION $VERSION...
-  echo '########################################################'
+  echo '###################################################################################################'
   echo ''
   
-  arduino --board $BOARD --verify ~/Arduino/espMQTT/espMQTT.ino --pref build.path=/tmp/espMQTT_build/ --pref build.extra_flags='-D'$targetname' -DESP8266 -DESPMQTT_BUILDSCRIPT -DESPMQTT_VERSION="'$VERSION'"'
+  arduino --board $BOARD --verify ~/Arduino/espMQTT/espMQTT.ino --pref build.path=/tmp/espMQTT_build/ --pref build.extra_flags='-D'$targetname' -DESP8266 -DWEBSOCKET_DISABLED=true -DASYNC_TCP_SSL_ENABLED -DUSE_HARDWARESERIAL -DESPMQTT_BUILDSCRIPT -DESPMQTT_VERSION="'$VERSION'"'
 
   if [ $? -ne 0 ]
   then 
   echo ''
-  echo '########################################################'
+  echo '###################################################################################################'
   echo BUILDING $targetname VERSION $VERSION FAILED!!!
-  echo '########################################################'
+  echo '###################################################################################################'
   echo ''
   exit $?
   fi
@@ -43,25 +43,27 @@ build ()
   mv /tmp/espMQTT_build/espMQTT.ino.bin '/tmp/espMQTT/'$VERSION'/'$targetname'_'$VERSION.bin
 
   echo ''
-  echo '########################################################'
+  echo '###################################################################################################'
   echo BUILDING $targetname VERSION $VERSION FINISHED.
-  echo '########################################################'
+  echo '###################################################################################################'
   echo ''
 }
 
+VERSION=$(git describe --tags)
 DIFFERENCE=$(git diff | wc -w)
-VERSION=$(head -n 1 ~/Arduino/espMQTT/version)
-VERSION=$(increment_version $VERSION)
-echo $VERSION > ~/Arduino/espMQTT/version
 
-#if [ $DIFFERENCE -eq 0 ]
-#then
-#	git tag v$VERSION 
-#	git push --tags
-#else
-#	echo "Not writing version to git tag because there are uncommited changes"
-#	exit 1
-#fi
+if [ $DIFFERENCE -eq 0 ]
+then
+	if [[ $VERSION == *"-"* ]]
+	then
+		VERSION=$(echo $VERSION | sed 's/-.*//' | sed 's/v//')
+		VERSION=$(increment_version $VERSION)
+	fi
+	git tag v$VERSION 
+	git push --tags
+else
+	VERSION=$VERSION-DIRTY
+fi
 
 mkdir -p /tmp/espMQTT/$VERSION
 rm -rf /tmp/espMQTT_build
@@ -69,14 +71,14 @@ mkdir /tmp/espMQTT_build
 
 
 declare -a TARGETS=("ESPMQTT_WEATHER" "ESPMQTT_AMGPELLETSTOVE" "ESPMQTT_BATHROOM" "ESPMQTT_BEDROOM2" "ESPMQTT_OPENTHERM" "ESPMQTT_SMARTMETER" "ESPMQTT_GROWATT" "ESPMQTT_SDM120" "ESPMQTT_WATERMETER" "ESPMQTT_DDNS" "ESPMQTT_GENERIC8266" "ESPMQTT_MAINPOWERMETER" "ESPMQTT_NOISE" "ESPMQTT_SOIL" "ESPMQTT_DIMMER")
-BOARD=esp8266:esp8266:nodemcuv2
+BOARD=esp8266com:esp8266:nodemcuv2
 for targetname in "${TARGETS[@]}"
 do
   build
 done
 
 declare -a TARGETS=("ESPMQTT_DUCOBOX" "ESPMQTT_SONOFFS20" "ESPMQTT_SONOFFBULB" "ESPMQTT_SONOFFPOWR2" "ESPMQTT_GARDEN" "ESPMQTT_SONOFF_FLOORHEATING" "ESPMQTT_IRRIGATION" "ESPMQTT_BLITZWOLF" "ESPMQTT_SONOFF4CH" "ESPMQTT_SONOFFDUAL" "ESPMQTT_SONOFFS20_PRINTER" "ESPMQTT_SONOFFPOW" "ESPMQTT_QSWIFIDIMMERD01" "ESPMQTT_QSWIFIDIMMERD02")
-BOARD=esp8266:esp8266:esp8285
+BOARD=esp8266com:esp8266:esp8285
 for targetname in "${TARGETS[@]}"
 do
   build
