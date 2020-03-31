dc4901bc952075e354134e444ba52c97f102ef55
jeroenst <jst@dse.nl>
2020-03-31 08:57:30 +0200

    moving old hardware and libraries to folder with timestamp

--- configure.sh
+++ configure.sh
@@ -6,8 +6,9 @@ git submodule foreach git reset --hard
 git reset --hard
 git checkout -f master
 patch hardware/esp8266com/esp8266/platform.txt < hardware/esp8266com_platform.txt.patch
-mv ../hardware ../hardware_espmqtt_backup
-mv ../libraries ../include_espmqtt_backup
+timestamp=$(date +%s)
+mv ../hardware ../hardware_espmqtt_backup_$timestamp
+mv ../libraries ../include_espmqtt_backup_$timestamp
 ln -s ~/Arduino/espMQTT/hardware ~/Arduino/hardware
 ln -s ~/Arduino/espMQTT/libraries ~/Arduino/libraries
 cd ./hardware/esp8266com/esp8266/tools/
