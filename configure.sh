5c1b536baff8f5caf8c93621f5ae122f133141a8
jeroenst <jst@dse.nl>
2020-03-31 08:53:44 +0200

    Removing local changes on start

--- configure.sh
+++ configure.sh
@@ -4,11 +4,12 @@ rm -rf ./hardware/esp8266com
 git submodule update --init --recursive
 git submodule foreach git reset --hard
 git reset --hard
+git checkout -f master
 patch hardware/esp8266com/esp8266/platform.txt < hardware/esp8266com_platform.txt.patch
 mv ../hardware ../hardware_espmqtt_backup
 mv ../libraries ../include_espmqtt_backup
 ln -s ~/Arduino/espMQTT/hardware ~/Arduino/hardware
 ln -s ~/Arduino/espMQTT/libraries ~/Arduino/libraries
-cd hardware/esp8266com/esp8266/tools/
+cd ./hardware/esp8266com/esp8266/tools/
 ./get.py
 cd ../../../..
