12ede2eba2be9239285b37cc2f4fb7de498d076f
JeroenSt <mail@jeroensteenhuis.nl>
2020-10-16 08:36:03 +0200

    Removed espMQTT_buildscript.h and fixed travis

--- build.sh
+++ build.sh
@@ -47,7 +47,7 @@ build ()
   -build-cache $HOME/Arduino/espMQTT/builds/cache \
   -prefs=build.warn_data_percentage=75 \
   -prefs="build.extra_flags=-DESP8266 -DWEBSOCKET_DISABLED=true -DASYNC_TCP_SSL_ENABLED -DUSE_HARDWARESERIAL -DESPMQTT_BUILDSCRIPT -DESPMQTT_VERSION=\"$VERSION\" -D$targetname" \
-  "$HOME/Arduino/espMQTT/espMQTT.ino"
+  \"$HOME/Arduino/espMQTT/espMQTT.ino\"
   
   set +x
 
