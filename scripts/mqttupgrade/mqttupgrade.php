b8009961f39242c942758e3a44119abd3ce14ae4
JeroenSt <mail@jeroensteenhuis.nl>
2021-02-09 16:04:48 +0100

    Moved topics to config file

--- scripts/mqttupgrade/mqttupgrade.php
+++ scripts/mqttupgrade/mqttupgrade.php
@@ -1,42 +1,15 @@
 <?php
 
 require("phpMQTT/phpMQTT.php");
+
+
+$devicetopics = array();
+
 require("mqttupgradesettings.php");
 
 $mqtt = new phpMQTT($server, $port, $client_id);
 if ($mqtt->connect(true, NULL, $username, $password)) {
 echo ("Connected to MQTT server\n");
-$devicetopics = array();
-
-$devicetopics[] = "home/SONOFF_WASHINGMACHINE";
-$devicetopics[] = "home/SONOFF_SERVER";
-$devicetopics[] = "home/SONOFF_POND";
-$devicetopics[] = "home/SONOFF_SHELTER";
-$devicetopics[] = "home/SONOFF_IRRIGATION";
-$devicetopics[] = "home/SONOFF_DUCOBOX";
-$devicetopics[] = "home/SONOFF_COFFEELAMP";
-$devicetopics[] = "home/SONOFF_FLOORHEATING";
-$devicetopics[] = "home/SONOFFS20_001";
-$devicetopics[] = "home/ESP_WEATHER";
-$devicetopics[] = "home/ESP_SDM120";
-$devicetopics[] = "home/ESP_PELLETSTOVE";
-$devicetopics[] = "home/BLITZWOLF_COFFEE";
-$devicetopics[] = "home/ESP_BEDROOM2";
-$devicetopics[] = "home/ESP_OPENTHERM";
-$devicetopics[] = "home/ESP_BATHROOM";
-$devicetopics[] = "home/ESP_WATERMETER";
-$devicetopics[] = "home/SONOFF_DISHWASHER";
-$devicetopics[] = "home/SONOFF_TV";
-$devicetopics[] = "home/QSWIFIDIMMER01";
-$devicetopics[] = "home/QSWIFIDIMMER02";
-$devicetopics[] = "home/QSWIFIDIMMER03";
-$devicetopics[] = "home/QSWIFIDIMMER04";
-$devicetopics[] = "home/QSWIFIDIMMER05";
-
-$devicetopics[] = "0002/ESP_SMARTMETER";
-$devicetopics[] = "0002/RELAY-CV";
-$devicetopics[] = "0002/BHT002-LIVINGROOM";
-$devicetopics[] = "0002/SONOFF_PONDTEMP";
 
 $devices = array();
 
