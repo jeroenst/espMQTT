df9657629c8c83513656452ab1842e5d259cfa6a
JeroenSt <nospam@nospam.org>
2022-02-06 12:54:28 +0100

    Changed refreshtime to refreshtimeout

--- ducobox.h
+++ ducobox.h
@@ -1,3 +1,3 @@
-void ducobox_init(uint8_t ducobox_relay0, uint8_t ducobox_relay1, uint8_t ducobox_refreshtime, void(*callback)(const char * ,String));
+void ducobox_init(uint8_t ducobox_relay0, uint8_t ducobox_relay1, uint8_t ducobox_refreshtimeout, void(*callback)(const char * ,String));
 void ducobox_setfan(uint8_t value);
 void ducobox_handle();
