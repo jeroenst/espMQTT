4fb3d81eda9096ad0fa748a39731aa2f3a9dade2
JeroenSt <nospam@nospam.org>
2022-04-21 08:56:47 +0200

    Adapted upgrade.sh to current configuration

--- upgrade.sh
+++ upgrade.sh
@@ -52,35 +52,35 @@ if [ "$2" == "" ]
 then
 	echo "Upgrading all devices..."
 #	upgrade $1 esp_odb1 ESPMQTT_OBD2
-	upgrade $1 esp_smartmeter ESPMQTT_SMARTMETER
-	upgrade $1 esp_sdm120 ESPMQTT_SDM120
-	upgrade $1 esp_growatt ESPMQTT_GROWATT
-	upgrade $1 esp_bathroom ESPMQTT_BATHROOM
-	upgrade $1 esp_bedroom2 ESPMQTT_BEDROOM2
-	upgrade $1 esp_opentherm ESPMQTT_OPENTHERM
-	upgrade $1 esp_pelletstove ESPMQTT_AMGPELLETSTOVE
-	upgrade $1 sonoff_tv ESPMQTT_SONOFFPOW
-	upgrade $1 sonoff_washingmachine ESPMQTT_SONOFFPOWR2
-	upgrade $1 sonoff_pond ESPMQTT_GARDEN
-	upgrade $1 sonoff_shelter ESPMQTT_GARDEN
-	upgrade $1 sonoff_irrigation ESPMQTT_IRRIGATION
-	upgrade $1 sonoff_ducobox ESPMQTT_DUCOBOX
-	upgrade $1 sonoff_coffeelamp ESPMQTT_SONOFFS20
-	upgrade $1 sonoff_server ESPMQTT_SONOFFPOWR2
+	upgrade $1 esp-smartmeter ESPMQTT_SMARTMETER
+	upgrade $1 esp-sdm120 ESPMQTT_SDM120
+	upgrade $1 esp-growatt2 ESPMQTT_GROWATT
+	upgrade $1 esp-bathroom ESPMQTT_BATHROOM
+	upgrade $1 esp-bedroom2 ESPMQTT_BEDROOM2
+	upgrade $1 esp-opentherm ESPMQTT_OPENTHERM
+	upgrade $1 esp-pelletstove ESPMQTT_AMGPELLETSTOVE
+	upgrade $1 sonoff-tv ESPMQTT_SONOFFPOW
+	upgrade $1 sonoff-washingmachine ESPMQTT_SONOFFPOWR2
+	upgrade $1 sonoff-pond ESPMQTT_GARDEN
+	upgrade $1 sonoff-shelter ESPMQTT_GARDEN
+	upgrade $1 sonoff-irrigation ESPMQTT_IRRIGATION
+	upgrade $1 sonoff-ducobox ESPMQTT_DUCOBOX
+	upgrade $1 sonoff-coffeelamp ESPMQTT_SONOFFS20
+	upgrade $1 sonoff-server ESPMQTT_SONOFFPOWR2
 #	upgrade $1 sonoff_bulb ESPMQTT_SONOFFBULB
-	upgrade $1 sonoffs20_001 ESPMQTT_SONOFFS20
-	upgrade $1 sonoff_floorheating ESPMQTT_SONOFF_FLOORHEATING
-	upgrade $1 esp_watermeter ESPMQTT_WATERMETER
-	upgrade $1 esp_weather ESPMQTT_WEATHER
+	upgrade $1 sonoffs20-001 ESPMQTT_SONOFFS20
+	upgrade $1 sonoff-floorheating ESPMQTT_SONOFF_FLOORHEATING
+	upgrade $1 esp-watermeter ESPMQTT_WATERMETER
+	upgrade $1 esp-weather ESPMQTT_WEATHER
 	upgrade $1 QSWIFIDIMMER01 ESPMQTT_QSWIFIDIMMERD02
 	upgrade $1 QSWIFIDIMMER02 ESPMQTT_QSWIFIDIMMERD02
 	upgrade $1 QSWIFIDIMMER03 ESPMQTT_QSWIFIDIMMERD02
-	upgrade $1 QSWIFIDIMMER04 ESPMQTT_QSWIFIDIMMERD01
+#	upgrade $1 QSWIFIDIMMER04 ESPMQTT_QSWIFIDIMMERD01
 	upgrade $1 QSWIFIDIMMER05 ESPMQTT_QSWIFIDIMMERD02
-	upgrade $1 sonoff_dishwasher ESPMQTT_SONOFFPOWR2
-	upgrade $1 blitzwolf_coffee ESPMQTT_BLITZWOLF
+#	upgrade $1 sonoff-dishwasher ESPMQTT_SONOFFPOWR2
+	upgrade $1 blitzwolf-coffee ESPMQTT_BLITZWOLF
 	upgrade $1 QSWIFISWITCH01 ESPMQTT_QSWIFISWITCH2C
-	upgrade $1 esp_livingroom ESPMQTT_LIVINGROOM
+	upgrade $1 esp-livingroom ESPMQTT_LIVINGROOM
 	exit 0
 fi
 
